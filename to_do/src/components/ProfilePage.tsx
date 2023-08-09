2e86794d8795b63200084b2e24fc41e239909678
naolt <naoltamrat11@gmai.com>
2023-08-09 08:57:24 +0300

    minor bug fix

--- to_do/src/components/ProfilePage.tsx
+++ to_do/src/components/ProfilePage.tsx
@@ -2,8 +2,6 @@ import React, { ReactElement } from "react";
 import { Profile } from "../model";
 
 const ProfilePage: React.FC<ReactElement> = () => {
-  profiles: Profile;
-
   return <div></div>;
 };
 
